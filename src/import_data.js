33c9d18e8ae3f66d3aab43cd54c697f1dee576eb
Miss Maria <contact@mariarosenholm.com>
2023-09-18 00:48:46 +0300

    The data transder fucntionaity ready, but performane issues since whole data is not transferred

--- src/import_data.js
+++ src/import_data.js
@@ -1,92 +1,54 @@
 import fs from "fs";
+
 import { parse } from "csv-parse";
 
-let columnNames = [];
-let tableData = [];
 let dataToJSON = [];
-let cleanData = [];
-let d = [];
+let data = [];
+let rawData = [];
 
-const Cleaning = (rawData, a) => {
-  let cleanedData;
-  let replaceValue;
-  if (a == "label") {
-    replaceValue = /[^a-zA-Z0-9 ]/g;
-  }
-  if (a == "tableD") {
-    replaceValue = /[^a-zA-zÄÖÜäöüß0-9 ]/g;
-  }
-  rawData.forEach((d) => {
-    cleanedData = d.replace(replaceValue, "");
-    if (a == "label") {
-      columnNames.push(cleanedData.trim());
-    } /*
-    if (a == "tableD") {
-      // cleanData.push(cleanedData).trim();
-    } */
-  });
-};
-
-const readLabels = new Promise((resolve, reject) => {
-  fs.createReadStream("./data-sources/person-small.csv")
-    .pipe(parse({ to: 1 }))
+const readData = new Promise((resolve, reject) => {
+  fs.createReadStream("./data-sources/person.csv")
+    .pipe(parse())
     .on("data", (row) => {
-      resolve(JSON.stringify(row, null, 2).split(";"));
+      let r = JSON.stringify(row, null, 2).split(";");
+      for (let i = 0; i < r.length + 1; i++) {
+        rawData.push(r[i]);
+      }
+      resolve("Data fetched!");
     });
 });
 
-const readData = new Promise((resolve, reject) => {
-  fs.createReadStream("./data-sources/person-small.csv")
-    .pipe(parse())
-    .on(
-      "data",
-      (row) =>
-        d.push(...JSON.stringify(row, null, 2).split(";")) + resolve("resolved")
-    );
-});
-
-const transferData = (columns, data) => {
-  readData.then(() => {
-    // Cleaning(d, "tableD");
-    // let columnP = 0;
-    //let dataObj = {};
-    let obj = {};
-    for (let i = 0; i < d.length - 1; i++) {
-      //tableData = d[i].replace(/[^a-zA-Z0-9 ]/g, "").trim();
-      tableData = d[i].replace(/[^a-zA-zÄÖÜäöüß0-9 ]/g, "").trim();
-      console.log(tableData);
-      for (let j = 1; j < tableData.length; j++) {
-        obj[columns[i]] = tableData;
-      }
-      data.push(obj);
+const Cleaning = () => {
+  rawData.forEach((d) => {
+    if (d !== void 0) {
+      d = d.replace(/[\[\]']+/g, "");
+      data.push(d.replace(/[^a-zA-zÄÖÜäöüß0-9 ]/g, "").trim());
     }
-    //console.log(data);
-
-    //fs.writeFileSync("./data/person.json", JSON.stringify(data, null, 2));
-
-    /* for (let i = 0; i < rawData.length + 1; i++) {
-      if (columnP === 5) {
-        columnP = 0;
-        dataObj[columns[columnP]] = rawData[i];
-        dataObj = {};
-      } else {
-        //console.log(rawData[i]);
-        dataObj[columns[columnP]] = rawData[i];
-        columnP = columnP + 1;
-
-        /*dataObj = Object.assign(
-              ...tableData.map((value) => {
-                ({ [columnNames[columnP]]: value });
-              })
-            ); 
-      } 
-    }*/
   });
+  let labelNames = data.slice(0, 5);
+  let values = data.slice(5, rawData.length);
+
+  return { labels: labelNames, values: values };
 };
 
-readLabels.then(async (data) => {
-  await Cleaning(data, "label");
-  transferData(columnNames, dataToJSON);
+const transferData = ({ labels, values }) => {
+  let labelIndex = 0;
+  let obj = {};
+  for (let i = 0; i < values.length; i++) {
+    obj[labels[labelIndex]] = values[i];
+    if (labelIndex < 4) {
+      labelIndex++;
+    } else {
+      labelIndex = 0;
+      dataToJSON.push(obj);
+      obj = {};
+    }
+  }
+  fs.writeFileSync("./data/person.json", JSON.stringify(dataToJSON, null, 2));
+};
+
+readData.then(() => {
+  transferData(Cleaning());
 });
 
 /*
