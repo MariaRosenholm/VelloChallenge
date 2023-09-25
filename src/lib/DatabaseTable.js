b4be1de2c484e5ebd632a063a90e8a05f140b70b
Miss Maria <contact@mariarosenholm.com>
2023-09-25 16:01:27 +0300

    fixed the random id generator

--- src/lib/DatabaseTable.js
+++ src/lib/DatabaseTable.js
@@ -47,8 +47,8 @@ export default class DatabaseTable {
 
     let arr = [];
     let obj = {
-      id: JSON.stringify(
-        Math.floor(Math.random() * (100000 - data.length + 1) + data.length + 1)
+      id: Math.floor(
+        Math.random() * (100000 - data.length + 1) + data.length + 1
       ),
       name: document.name,
       age: document.age,
